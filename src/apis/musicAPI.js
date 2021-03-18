b794e1841cfeab945d31f98a5a7d134f42d79ca7
Jozo Suton <61884667+jsuton00@users.noreply.github.com>
2021-03-18 20:28:38 +0100

    update access-control-allow-origin

--- src/apis/musicAPI.js
+++ src/apis/musicAPI.js
@@ -3,5 +3,10 @@ import axios from 'axios';
 export const searchSongs = async (searchTerm) => {
 	return await axios.get(
 		`https://itunes.apple.com/search?term=${searchTerm}&media=music&entity=musicTrack`,
+		{
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+		},
 	);
 };
